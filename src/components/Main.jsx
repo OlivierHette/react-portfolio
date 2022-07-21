869ad7b2141d21617ec39f0cef3e3fc6859e9718
Olivier Hette <olivierhette13890@gmail.com>
2022-07-21 16:45:29 +0200

    [FEATURE] Add of hero and about section's html and styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,41 +1,112 @@
 import pdpImg from '../images/profile-image.webp'
 
+const aboutText = "Développeur full stack, je me suis formé en autodidacte depuis mes 11 ans. J’ai décidé de formaliser mes connaissances et mon experience par un diplome de développeur Full Stack."
+
 function Main() {
   
   return(
     <main>
-      <div className="section hero">
+      <section className="section hero">
         <div className="hero__intro">
           <span>Bonjour, je suis</span>
           <h1>Olivier Hette</h1>
           <h2>Développeur Full Stack</h2>
         </div>
-        <div className="hero_btn">
+        <div className="hero__btn">
           <div className="btn btn--border-only">
             <span>Télecharger CV</span>
           </div>
-          <div className="btn btn--radius">
+          <div className="btn btn--bg">
             <span>À propos</span>
           </div>
         </div>
-        <img src={pdpImg} alt="visage olivier hette" />
-      </div>
+        <img className="image image--rounded" src={pdpImg} alt="visage olivier hette" />
+      </section>
 
-      <div className="section about">
+      <section className="section about">
+        <div className="section__title">
+          <span className='text-xs'>Introduction</span>
+          <h3>À propos</h3>
+        </div>
 
-      </div>
+        <img className="image image--radius" src={pdpImg} alt="visage olivier hette" />
 
-      <div className="section experiences">
+        <div className="about__container">
+          <div className="about__item">
+            <i class='bx bx-code-curly bx-sm' ></i>
+            <div className="about__item-text">
+              <h4>Experiences</h4>
+              <span className='text-xs'>+ 1 an</span>
+            </div>
+          </div>
 
-      </div>
+          <div className="about__item">
+          <i className='bx bx-briefcase-alt bx-sm' ></i>
+            <div className="about__item-text">
+              <h4>Missions</h4>
+              <span className='text-xs'>+ 10 projets</span>
+            </div>
+          </div>
 
-      <div className="section projects">
+          <div className="about__item">
+            <i class='bx bxs-graduation bx-sm'></i>
+            <div className="about__item-text">
+              <h4>Formation</h4>
+              <span className='text-xs'>Bac +2</span>
+            </div>
+          </div>
+        </div>
 
-      </div>
+        <p className='about__content-text'>{aboutText}</p>
+      </section>
 
-      <div className="section contact">
+      <section className="section experiences">
+        <div className="section__title">
+          <span className='text-xs'>mes compétences</span>
+          <h3>Mon experiences</h3>
+        </div>
 
-      </div>
+        <div className="experiences__container">
+          <div className="experiences__card">
+            <h3>FrontEnd</h3>
+
+            <div className="experiences__ability">
+              <i class='bx bxs-badge-check bx-sm'></i>
+              <div className="experiences__ability-text">
+                <h4>HTML5</h4>
+                <span></span>
+              </div>
+            </div>
+
+          </div>
+           
+          <div className="experiences__card">
+            <h3>BackEnd</h3>
+            <div className="experiences__ability">
+            <i class='bx bxs-badge-check bx-sm'></i>
+              <div className="experiences__ability-text">
+                <h4>NodeJs</h4>
+                <span></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="projects">
+        <div className="section__title">
+          <span className='text-xs'>Introduction</span>
+          <h3>À propos</h3>
+        </div>
+
+      </section>
+
+      <section className="contact">
+        <div className="section__title">
+          <span className='text-xs'>Introduction</span>
+          <h3>À propos</h3>
+        </div>
+      </section>
     </main>
   )
 }
